bf585d15e699ae2213d5c15e0a3f062465e80fd2
sai109 <lukebraithwaite7@gmail.com>
2019-03-24 10:07:39 +0000

    Set up serving of html

--- server/server.js
+++ server/server.js
@@ -5,8 +5,10 @@ const app = express()
 
 const publicPath = path.join(__dirname, '../public');
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.listen(process.ENV.port || 3000);
+app.listen(process.env.PORT || 3000);
